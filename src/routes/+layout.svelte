1db539f2d52a6554e8a9d2f09d1fc5318eb6db08
lnicoletti <leo.nicoletti@hotmail.fr>
2024-03-02 23:51:37 +0100

    debugging

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,19 +32,21 @@
 />
 <!-- class="bg-white-100 dark:bg-slate-700 border-b border-slate-200 dark:border-slate-900"  -->
 <div >
-	<!-- {#if mobile}
-		<Sidebar bind:open/>
-		<NavMobile bind:sidebar={open}/>
-		<main>
-			<slot />
-		</main>
-	{:else} -->
-		<Header />
-		<Nav />
-		<main>
-			<slot />
-		</main>
-	<!-- {/if} -->
+	{#if innerWidth != null}
+		{#if mobile}
+			<Sidebar bind:open/>
+			<NavMobile bind:sidebar={open}/>
+			<main>
+				<slot />
+			</main>
+		{:else}
+			<Header />
+			<Nav />
+			<main>
+				<slot />
+			</main>
+		{/if}
+	{/if}
 
 	
 
