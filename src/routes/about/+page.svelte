d827d33627626b245de020e80515d1c0c4e53002
lnicoletti <leo.nicoletti@hotmail.fr>
2024-03-03 05:07:14 +0100

    debugging

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -67,6 +67,7 @@ My work has been cited by <a href="https://video.ethz.ch/events/2023/computation
     'cusp.svg',
     'ubc.png',
   ];
+  let screenWidth;
   $: screenWidth = 1024;
   $: vWideScreen = screenWidth > 1440;
   $: laptop = screenWidth >= 1024;
