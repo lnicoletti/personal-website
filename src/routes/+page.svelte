2a34707581fb7fb1f5e023e5e2cea2fa1ef3dbbf
lnicoletti <leo.nicoletti@hotmail.fr>
2024-07-03 03:27:13 +0200

    autoplay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -75,7 +75,7 @@
           {#if data.img.split(".")[1] === "mp4"}
             <a href={data.url} target="__blank">
               <!-- svelte-ignore a11y-media-has-caption -->
-              <video width="100%" height="100%" autoplay loop>
+              <video width="100%" height="100%" autoplay loop muted>
                 <source src="/images/{data.img}" type="video/mp4" />
                 <source src="/images/{data.img.replace(".mp4",".ogg")}" type="video/ogg" />
               </video>
